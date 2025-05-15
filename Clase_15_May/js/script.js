6c4aa2e918db0ec1ee230f8cb87a9bba69f77ee1
JudithMallama <judith.mallama@udea.edu.co>
2025-05-15 09:28:59 -0500

    Clase May 15 - hoja de estilo en cascada - CSS

---
+++ Clase_15_May/js/script.js
@@ -0,0 +1,11 @@
+function resaltar(elemento){
+    elemento.style.backgroundColor = "blue";
+    elemento.style.fontWeight = "bold";
+    elemento.style.border="2px solid white";
+}
+
+function restaurar(elemento){
+    elemento.style.backgroundColor = "transparent";
+    elemento.style.fontWeight =  "normal";
+    elemento.style.border= "none";
+}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Material de clase
 Link HTML  https://www.w3schools.com/
 Link Bootstrap  https://getbootstrap.com
+Link de colores para css https://htmlcolorcodes.com/es/
+Documentación de  css : https://developer.mozilla.org/es/docs/Learn_web_development/Core/Styling_basics/Box_model
